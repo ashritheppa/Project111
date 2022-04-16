a7b7990c16d2b52255934bedbf3adc20c0a70ad0
ashritheppa <84898857+ashritheppa@users.noreply.github.com>
2022-04-16 12:16:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+Prediction;
+Webcam.set({
+width: 350,
+height: 300,
+image_format: 'png',
+png_quality: 90
+});
+camera = document.getElementById("camera");
+Webcam.attach('#camera');
+function take_snapshot()
+{
+Webcam.snap(function(data_uri){
+document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+});
+}
+console.log('ml5 version:', ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/Lh-0tgu5r/', modelLoaded);
+function modelLoaded() {
+console.log('Model Loaded!');
+}
+function speak(){
+var synth = window.speechSynthesis;
+speak_data_1 = "My prediction is" + Prediction;
+var utterThis = new SpeechSynthesisUtterance(Prediction);
+synth.speak(utterThis);
+}
